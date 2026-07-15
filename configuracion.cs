ed2ee8a49c50b843f697f34ecac8850672559fe3
AxelDeJesuuHernandezCoronado <xlhernandezcoronado@gmail.com>
2026-07-14 19:45:55 -0600

    nada

--- configuracion.cs
+++ configuracion.cs
@@ -11,86 +11,86 @@ using System.Windows.Forms;
 
 namespace pryCafeteriaEscolar
 {
-    public partial class configuracion : UserControl
-    {
+    //public partial class configuracion : UserControl
+    //{
 
-        public configuracion()
-        {
-            InitializeComponent();
-        }
+    //    public configuracion()
+    //    {
+    //        InitializeComponent();
+    //    }
 
-        private void configuracion_Load(object sender, EventArgs e)
-        {
-            CambiarTema();
-        }
+    //    private void configuracion_Load(object sender, EventArgs e)
+    //    {
+    //        CambiarTema();
+    //    }
 
-        private void CambiarColorControles(Control padre, Color fondo, Color texto)
-        {
-            foreach (Control c in padre.Controls)
-            {
-                c.BackColor = fondo;
-                c.ForeColor = texto;
+    //    private void CambiarColorControles(Control padre, Color fondo, Color texto)
+    //    {
+    //        foreach (Control c in padre.Controls)
+    //        {
+    //            c.BackColor = fondo;
+    //            c.ForeColor = texto;
 
-                if (c.HasChildren)
-                {
-                    CambiarColorControles(c, fondo, texto);
-                }
-            }
-        }
-        private void CambiarTema()
-        {
-            if (trackTemas.Value == 0)
-            {
-                this.BackColor = Color.WhiteSmoke;
-                CambiarColorControles(this, Color.WhiteSmoke, Color.Black);
-            }
-            else
-            {
-                this.BackColor = Color.FromArgb(45, 45, 48);
-                CambiarColorControles(this, Color.FromArgb(45, 45, 48), Color.White);
-            }
-        }
+    //            if (c.HasChildren)
+    //            {
+    //                CambiarColorControles(c, fondo, texto);
+    //            }
+    //        }
+    //    }
+    //    private void CambiarTema()
+    //    {
+    //        if (trackTemas.Value == 0)
+    //        {
+    //            this.BackColor = Color.WhiteSmoke;
+    //            CambiarColorControles(this, Color.WhiteSmoke, Color.Black);
+    //        }
+    //        else
+    //        {
+    //            this.BackColor = Color.FromArgb(45, 45, 48);
+    //            CambiarColorControles(this, Color.FromArgb(45, 45, 48), Color.White);
+    //        }
+    //    }
 
-        private void trackTemas_Scroll(object sender, EventArgs e)
-        {
-            CambiarTema();
-        }
+    //    private void trackTemas_Scroll(object sender, EventArgs e)
+    //    {
+    //        CambiarTema();
+    //    }
 
-        private void cmbbxFuente_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            float tamaño = float.Parse(cmbbxFuente.Text);
+    //    private void cmbbxFuente_SelectedIndexChanged(object sender, EventArgs e)
+    //    {
+    //        float tamaño = float.Parse(cmbbxFuente.Text);
 
-            CambiarFuente(this, tamaño);
-        }
-        private void CambiarFuente(Control padre, float tamaño)
-        {
-            foreach (Control c in padre.Controls)
-            {
-                c.Font = new Font(c.Font.FontFamily, tamaño, c.Font.Style);
+    //        CambiarFuente(this, tamaño);
+    //    }
+    //    private void CambiarFuente(Control padre, float tamaño)
+    //    {
+    //        foreach (Control c in padre.Controls)
+    //        {
+    //            c.Font = new Font(c.Font.FontFamily, tamaño, c.Font.Style);
 
-                if (c.HasChildren)
-                {
-                    CambiarFuente(c, tamaño);
-                }
-            }
-        }
+    //            if (c.HasChildren)
+    //            {
+    //                CambiarFuente(c, tamaño);
+    //            }
+    //        }
+    //    }
 
-        // Handlers adicionales referenciados desde el diseñador
-        private void groupBox3_Enter(object sender, EventArgs e)
-        {
-            // Intencionalmente vacío
-        }
+    //    // Handlers adicionales referenciados desde el diseñador
+    //    private void groupBox3_Enter(object sender, EventArgs e)
+    //    {
+    //        // Intencionalmente vacío
+    //    }
 
-        private void label7_Click(object sender, EventArgs e)
-        {
-            // Intencionalmente vacío
-        }
+    //    private void label7_Click(object sender, EventArgs e)
+    //    {
+    //        // Intencionalmente vacío
+    //    }
 
 
-        private void label4_Click(object sender, EventArgs e)
-        {
-            // Intencionalmente vacío
-        }
-        // InitializeComponent y la definición de controles están en configuracion.Designer.cs
-    }
+    //    private void label4_Click(object sender, EventArgs e)
+    //    {
+    //        // Intencionalmente vacío
+    //    }
+    //    // InitializeComponent y la definición de controles están en configuracion.Designer.cs
+    //}
 }
