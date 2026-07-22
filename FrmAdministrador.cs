dcbc2136004626705675fb8915239564cde42de9
Javier de jesus Ramirez Hernandez <javierdejesusrmzhdez@gmail.com>
2026-07-21 19:46:52 -0600

    cambio en el diseno de productos

--- FrmAdministrador.cs
+++ FrmAdministrador.cs
@@ -69,6 +69,7 @@ namespace pryCafeteriaEscolar
             productos.Dock = DockStyle.Fill;
 
             // 4. Lo agregamos al panel y lo mostramos
+            splitContainer1.Panel2.Controls.Clear();
             splitContainer1.Panel2.Controls.Add(productos);
             productos.Show();
         }
